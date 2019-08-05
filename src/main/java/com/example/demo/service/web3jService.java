49a0090e2b6bd6ebadc7412cfb2cde97115be270
Robin <hongxing525@126.com>
2019-08-05 23:45:20 +0800

    add depency for ethereum dev

---
+++ src/main/java/com/example/demo/service/web3jService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import com.example.demo.mapper.DemoMapper;
+import com.example.demo.model.DemoModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class web3jService {
+
+    private final DemoMapper dao;
+
+    @Autowired
+    public web3jService(DemoMapper dao) {
+        this.dao = dao;
+    }
+
+    public void web3jGeth() {
+        
+    }
+    public boolean insert(DemoModel model) {
+        return dao.insert(model) > 0;
+    }
+
+    public DemoModel select(int id) {
+        return dao.select(id);
+    }
+
+    public List<DemoModel> selectAll() {
+        return dao.selectAll();
+    }
+
+    public boolean updateValue(DemoModel model) {
+        return dao.updateValue(model) > 0;
+    }
+
+    public boolean delete(Integer id) {
+        return dao.delete(id) > 0;
+    }
+}
